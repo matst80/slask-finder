dd6b53e4398e190a3db4d2a769720f523e01c3bf
matst80 <mats.tornberg@gmail.com>
2024-10-08 14:27:07 +0200

    remove clickhouse

---
+++ pkg/tracking/rabbit-tracking.go
@@ -0,0 +1,147 @@
+package tracking
+
+import (
+	"encoding/json"
+	"net/http"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"tornberg.me/facet-search/pkg/index"
+)
+
+type RabbitTrackingConfig struct {
+	TrackingTopic string
+	Url           string
+}
+
+type RabbitTracking struct {
+	RabbitTrackingConfig
+	connection *amqp.Connection
+	channel    *amqp.Channel
+}
+
+func NewRabbitTracking(config RabbitTrackingConfig) *RabbitTracking {
+	ret := RabbitTracking{
+		RabbitTrackingConfig: config,
+	}
+	ret.Connect()
+	return &ret
+}
+
+func (t *RabbitTracking) Connect() error {
+
+	conn, err := amqp.Dial(t.Url)
+	if err != nil {
+		return err
+	}
+	t.connection = conn
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	t.channel = ch
+
+	return nil
+}
+
+func (t *RabbitTracking) Close() error {
+	defer t.connection.Close()
+	return t.channel.Close()
+}
+
+func (t *RabbitTracking) send(data any) error {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	return t.channel.Publish(
+		t.TrackingTopic,
+		t.TrackingTopic,
+		true,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        bytes,
+		},
+	)
+}
+
+type Session struct {
+	SessionId uint32 `json:"session_id"`
+	UserAgent string `json:"user_agent"`
+	Ip        string `json:"ip"`
+	Language  string `json:"language"`
+}
+
+func (rt *RabbitTracking) TrackSession(session_id uint32, r *http.Request) error {
+	return rt.send(Session{
+		SessionId: session_id,
+		Language:  r.Header.Get("Accept-Language"),
+		UserAgent: r.UserAgent(),
+		Ip:        r.RemoteAddr,
+	})
+}
+
+type Event struct {
+	SessionId uint32  `json:"session_id"`
+	Event     uint16  `json:"event"`
+	Item      uint    `json:"item"`
+	Position  float32 `json:"position"`
+}
+
+type CartEvent struct {
+	SessionId uint32 `json:"session_id"`
+	Event     uint16 `json:"event"`
+	Item      uint   `json:"item"`
+	Quantity  uint   `json:"quantity"`
+}
+
+type SearchEventData struct {
+	*index.Filters
+	Event uint16 `json:"event"` // 1
+	Query string `json:"query"`
+}
+
+func (rt *RabbitTracking) TrackSearch(session_id uint32, filters *index.Filters, query string) error {
+	return rt.send(&SearchEventData{
+		Event:   1,
+		Filters: filters,
+		Query:   query,
+	})
+
+}
+
+func (rt *RabbitTracking) TrackClick(session_id uint32, item_id uint, position float32) error {
+	return rt.send(&Event{
+		SessionId: session_id,
+		Event:     2,
+		Item:      item_id,
+		Position:  position,
+	})
+}
+
+func (rt *RabbitTracking) TrackAddToCart(session_id uint32, item_id uint, quantity uint) error {
+	return rt.send(&CartEvent{
+		SessionId: session_id,
+		Event:     3,
+		Item:      item_id,
+		Quantity:  quantity,
+	})
+}
+
+func (rt *RabbitTracking) TrackPurchase(session_id uint32, item_id uint, quantity uint) error {
+	return rt.send(&CartEvent{
+		SessionId: session_id,
+		Event:     4,
+		Item:      item_id,
+		Quantity:  quantity,
+	})
+}
+
+func (rt *RabbitTracking) TrackImpression(session_id uint32, item_id uint, position float32) error {
+	return rt.send(&Event{
+		SessionId: session_id,
+		Event:     5,
+		Item:      item_id,
+		Position:  position,
+	})
+}
