71d2db297f8f4725dcc699d83753cd91da54fa1e
matst80 <mats.tornberg@gmail.com>
2024-10-08 11:08:32 +0200

    tracking interface

--- pkg/tracking/tracking.go
+++ pkg/tracking/tracking.go
@@ -12,6 +12,15 @@ import (
 	"tornberg.me/facet-search/pkg/index"
 )
 
+type Tracking interface {
+	TrackSession(session_id uint32, r *http.Request) error
+	TrackSearch(session_id uint32, filters *index.Filters, query string) error
+	TrackClick(session_id uint32, item_id uint, position float32) error
+	TrackAddToCart(session_id uint32, item_id uint, quantity uint) error
+	TrackPurchase(session_id uint32, item_id uint, quantity uint) error
+	TrackImpression(session_id uint32, item_id uint, position float32) error
+}
+
 type ClickHouse struct {
 	Conn driver.Conn
 }
