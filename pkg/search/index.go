81a177493985b5f8cae5badd25bf8b20b8b4395a
matst80 <mats.tornberg@gmail.com>
2025-04-23 20:26:47 +0200

    change mergelimit

--- pkg/search/index.go
+++ pkg/search/index.go
@@ -204,7 +204,7 @@ func (i *FreeTextIndex) getBestFuzzyMatch(token Token, max int) []Token {
 
 // TODO maybe two itemlists, one for exact and one for fuzzy
 
-const MERGE_LIMIT = 80
+const MERGE_LIMIT = 1
 
 func (i *FreeTextIndex) Search(query string) *types.ItemList {
 	res := &types.ItemList{}
