c14ed7435a320c9be7c684a6c3a00fca45ea3996
matst80 <mats.tornberg@gmail.com>
2025-05-09 15:12:49 +0200

    repair

--- pkg/server/webserver_utils.go
+++ pkg/server/webserver_utils.go
@@ -3,6 +3,7 @@ package server
 import (
 	"encoding/json"
 	"log"
+	"maps"
 	"net/http"
 	"sync"
 
@@ -286,23 +287,23 @@ func (ws *WebServer) getSuggestFacets(baseIds *types.ItemList, sr *types.FacetRe
 func (ws *WebServer) getOtherFacets(baseIds *types.ItemList, sr *types.FacetRequest, ch chan *index.JsonFacet, wg *sync.WaitGroup) {
 
 	fieldIds := make(map[uint]struct{})
-	limit := 50
+	limit := 30
 	resultCount := len(*baseIds)
-	//if resultCount > 65535 {
-	//limit = 20
-	for id, f := range ws.Index.Facets {
-		if !f.IsExcludedFromFacets() && !sr.IsIgnored(id) {
-			fieldIds[id] = struct{}{}
+	if resultCount > 32000 {
+		limit = 20
+		for id, f := range ws.Index.Facets {
+			if !f.IsExcludedFromFacets() && !sr.IsIgnored(id) {
+				fieldIds[id] = struct{}{}
+			}
+		}
+	} else {
+		for id := range *baseIds {
+			itemFieldIds, ok := ws.Index.ItemFieldIds[id]
+			if ok {
+				maps.Copy(fieldIds, itemFieldIds)
+			}
 		}
 	}
-	//} else {
-	//	for id := range *baseIds {
-	//		itemFieldIds, ok := ws.Index.ItemFieldIds[id]
-	//		if ok {
-	//			maps.Copy(fieldIds, itemFieldIds)
-	//		}
-	//	}
-	//}
 	count := 0
 	//var base *types.BaseField = nil
 	if resultCount == 0 {
